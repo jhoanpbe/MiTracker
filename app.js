a37f3084655e3fdcc3892449368dc5cc341bad3d
jhoanpbe <162043953+jhoanpbe@users.noreply.github.com>
2026-08-12 14:57:56 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const button = document.getElementById("startButton");
+const status = document.getElementById("status");
+
+const ctx = canvas.getContext("2d");
+
+async function startCamera() {
+
+    try {
+
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: {
+                facingMode: "user"
+            },
+            audio: false
+        });
+
+        video.srcObject = stream;
+
+        status.textContent = "Cámara funcionando";
+
+    } catch (error) {
+
+        console.error(error);
+
+        status.textContent =
+            "No se pudo acceder a la cámara";
+
+    }
+}
+
+button.addEventListener(
+    "click",
+    startCamera
+);let stream = null;
+let usandoFrontal = false;
+
+async function iniciarCamara() {
+    if (stream) {
+        stream.getTracks().forEach(track => track.stop());
+    }
+
+    stream = await navigator.mediaDevices.getUserMedia({
+        video: {
+            facingMode: usandoFrontal ? "user" : "environment"
+        },
+        audio: false
+    });
+
+    const video = document.getElementById("camera");
+    video.srcObject = stream;
+}
+
+async function cambiarCamara() {
+    usandoFrontal = !usandoFrontal;
+    await iniciarCamara();
+}
